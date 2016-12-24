264f59a7c1fe77350670fb2c2587a2c5f7d7f1c5
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-12-24 18:43:47 -0500

    Implement 'tree_lights' in 'twinkle_tree'

--- twinkle_tree.py
+++ twinkle_tree.py
@@ -3,7 +3,7 @@
 
 import random
 import time
-from dotstar import Adafruit_DotStar
+from dotstar import tree_lights
 
 # Module information:
 __author__ = "Lucien Gaitskell"
@@ -13,43 +13,19 @@ __maintainer__ = "Lucien Gaitskell"
 __status__ = "Development"
 
 
-# 3 Strips + Disk + Square:
-numpixels = 3*300+255+256
-
 # Initialize the strip:
-strip = Adafruit_DotStar(numpixels, 4000000)  # slower rate
-
-strip.begin()            # Initialize pins for output
-strip.setBrightness(64)  # Limit brightness to ~1/4 duty cycle
-
-# List bad pixels to ignore:
-badPixels = [593, 594]
-
-
-# Equivalent to 'strip.setPixelColor' with bad pixel ignoring:
-def __setPixel(px, *args, **kwargs):
-    if px not in badPixels:
-        strip.setPixelColor(px, *args, **kwargs)
-
-
-# Shut down all pixels:
-def shutDownAllPixels():
-    """Shut down all lights on the tree."""
-    for px in range(numpixels):
-        if px not in badPixels:
-            __setPixel(px, 0, 0, 0)
-    strip.show()
+tree = tree_lights.TreeLights()  # slower rate
 
 
 # Main 'run' function:
 def run():
     """Twinkle the lights on the tree."""
     while True:
-        for px in range(numpixels):
+        for px in range(tree_lights.NUM_PIXELS):
             # Get a random number in a specific range:
             randomNumb = random.randrange(4, 33, 1)
-            __setPixel(px, 0, int(randomNumb*1.5), randomNumb)
-        strip.show()
+            tree.setPixel(px, 0, int(randomNumb*1.5), randomNumb)
+        tree.show()
         time.sleep(0.1)
 
 
@@ -59,4 +35,4 @@ if __name__ == "__main__":
         run()
     except KeyboardInterrupt:
         # Shutdown pixels on interrupt:
-        shutDownAllPixels()
+        tree.zeroAllPixels()
